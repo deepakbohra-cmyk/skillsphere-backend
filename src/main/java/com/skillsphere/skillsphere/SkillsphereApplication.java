f80d418e4bf6d16ad26181a92564a825202722c6
deepak <deepak.bohra@vacobinary.in>
2026-05-14 10:28:06 +0000

    setup

---
+++ src/main/java/com/skillsphere/skillsphere/SkillsphereApplication.java
@@ -0,0 +1,13 @@
+package com.skillsphere.skillsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsphereApplication.class, args);
+	}
+
+}
